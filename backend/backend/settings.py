66a6da79c3e6dbb079ba25c6791e9bfcaee71bdb
Филимонова Екатерина <filimonka@yandex.ru>
2022-08-26 14:26:33 +0300

    some stranges with settings

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -17,7 +17,7 @@ SECRET_KEY = '(qsowcco7skm+aaezlzk*-2ydplp7m7j16%hi2(6tmpbxiup4@'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['158.160.9.78', 'localhost', '127.0.0.1', 'backend']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'backend', '158.160.9.78']
 
 
 # Application definition
